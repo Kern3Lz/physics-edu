f858ac65bef9cce890bc4eeef4bbd8b06f66121f
KenZundera <fauzansuken@gmail.com>
2024-06-10 11:41:10 +0700

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,29 @@
-user = {
-  username: "admin",
-  password: "admin",
-};
+let user = [
+  {
+    username: "admin",
+    password: "admin",
+  },
+  {
+    username: "user",
+    password: "user",
+  },
+];
 
 function login() {
   let username = document.getElementById("username").value;
   let pass = document.getElementById("password").value;
 
-  if (username == user.username && pass == user.password) {
-    localStorage.setItem("username", username);
-    localStorage.setItem("login", true);
-    alert("Login berhasil");
-    window.location.href = "index.html";
-  } else {
-    alert("Username atau password salah");
+  for (let i = 0; i < user.length; i++) {
+    if (username === user[i].username && pass === user[i].password) {
+      localStorage.setItem("username", username);
+      localStorage.setItem("login", true);
+      alert("Login berhasil");
+      window.location.href = "index.html";
+      return;
+    } else {
+      alert("Username atau password salah");
+      return;
+    }
   }
 }
 
